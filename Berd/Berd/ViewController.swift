acdb44ad0109af9c0045bec67f8c2e1250a17ef4
Aaron Parks <aaparks@csumb.edu>
2019-10-23 18:48:33 -0700

    created xcode project

---
+++ Berd/Berd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Berd
+//
+//  Created by Aaron Parks on 10/23/19.
+//  Copyright © 2019 Aaron Parks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
